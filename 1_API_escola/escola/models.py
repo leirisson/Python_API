7b6bc544de18e99d0d859600b749b4501a94a333
leirisson <leirissonsouza99@gmail.com>
2024-11-18 16:37:20 -0400

    djangoRestAPI

--- 1_API_escola/escola/models.py
+++ 1_API_escola/escola/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+'''
+
+ID
+Nome
+E-mail
+CPF
+Máxima 11 caracteres
+Data de nascimento
+Número de celular
+Máximo de 14 caracteres 
+
+'''
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, max_length=30)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    numero_celular = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+    
